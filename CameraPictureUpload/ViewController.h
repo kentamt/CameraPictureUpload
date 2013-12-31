f825432e1d40a587526656073f8ca4292981ec68
kkkmmm <kentabell0725@gmail.com>
2013-12-31 17:17:58 +0900

    initial commit."

---
+++ CameraPictureUpload/ViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  CameraPictureUpload
+//
+//  Created by 岡内和博 on 12/10/29.
+//  Copyright (c) 2012年 Okahiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIPopoverControllerDelegate>
+{
+	UIPopoverController *popOver;
+	CGPoint touchPoint;
+}
+
+- (IBAction)cameraButtonTapped:(id)sender;
+- (IBAction)libraryButtonTapped:(id)sender;
+- (IBAction)uploadButtonTapped:(id)sender;
+
+@property (retain, nonatomic) IBOutlet UIImageView *pictureImage;
+
+@end
